c14cf75850d729fdcc232ab170cc59175e6ccb66
Konstantin S. <havendv@gmail.com>
2026-05-05 18:33:09 +0400

    feat: Updated OpenAPI spec (#5)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Mureka/Generated/Mureka.ILyricsClient.GenerateLyrics.g.cs
+++ src/libs/Mureka/Generated/Mureka.ILyricsClient.GenerateLyrics.g.cs
@@ -19,6 +19,18 @@ namespace Mureka
         /// <summary>
         /// Generate lyrics from a prompt
         /// </summary>
+        /// <param name="request"></param>
+        /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
+        /// <param name="cancellationToken">The token to cancel the operation with</param>
+        /// <exception cref="global::Mureka.ApiException"></exception>
+        global::System.Threading.Tasks.Task<global::Mureka.AutoSDKHttpResponse<global::Mureka.GenerateLyricsResponse>> GenerateLyricsAsResponseAsync(
+
+            global::Mureka.GenerateLyricsRequest request,
+            global::Mureka.AutoSDKRequestOptions? requestOptions = default,
+            global::System.Threading.CancellationToken cancellationToken = default);
+        /// <summary>
+        /// Generate lyrics from a prompt
+        /// </summary>
         /// <param name="prompt"></param>
         /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
